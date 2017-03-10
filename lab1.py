3362f60a9a77ccf15c1f0a39b1fd66b6cdfec54f
ba50 <barjas02@gmail.com>
2017-03-10 09:01:45 +0100

    pierwszy commit

--- lab1.py
+++ lab1.py
@@ -3,11 +3,14 @@
 import numpy as np
 from numpy import sin, cos, pi, sqrt
 from scipy.integrate import quad
+import itertools
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
 A = 15
 a = 10/3
 V_0 = 2.5
-n = 3
+n = 20
 x_max = 15
 
 def V(x):
@@ -34,14 +37,32 @@ def cos_integrate(x, i, j, A):
 
 even = np.zeros((n, n))
 odd = np.zeros((n, n))
-
+"""
 for index, x in np.ndenumerate(even):
-    even[index[0]][index[1]]    = quad(sin_integrate, -x_max, x_max, args=(index[0], index[1], A))[0]
+    even[index[0]][index[1]]    = quad(sin_integrate, -x_max, x_max, args=(index[0]+1, index[1]+1, A))[0]
     odd[index[0]][index[1]]     = quad(cos_integrate, -x_max, x_max, args=(index[0], index[1], A))[0]
+"""
+for i in range(0,5):
+    for j in range(0,5):
+        even[i][j]    = quad(sin_integrate, -x_max, x_max, args=(i+1, j+1, A))[0]
+        odd[i][j]     = quad(cos_integrate, -x_max, x_max, args=(i, j, A))[0]
+for i in range(5,10):
+    for j in range(5,10):
+        even[i][j]    = quad(sin_integrate, -x_max, x_max, args=(i+1, j+1, A))[0]
+        odd[i][j]     = quad(cos_integrate, -x_max, x_max, args=(i, j, A))[0]
+for i in range(10,15):
+    for j in range(10,15):
+        even[i][j]    = quad(sin_integrate, -x_max, x_max, args=(i+1, j+1, A))[0]
+        odd[i][j]     = quad(cos_integrate, -x_max, x_max, args=(i, j, A))[0]
+for i in range(15,20):
+    for j in range(15,20):
+        even[i][j]    = quad(sin_integrate, -x_max, x_max, args=(i+1, j+1, A))[0]
+        odd[i][j]     = quad(cos_integrate, -x_max, x_max, args=(i, j, A))[0]
 
-H = np.zeros((2*n,2*n))
-for id in range(0,n):
-        H[2*id][2*id] = even[id][id]
-        H[2*id+1][2*id+1] = odd[id][id]
-print(H)
+w_even, v_even = LA.eig(even)
+w_odd, v_odd = LA.eig(odd)
+
+plt.plot(range(1,n+1),w_even)
+plt.plot(range(1,n+1),w_odd)
+plt.show()
 
