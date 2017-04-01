4aee3ef99cf116011f1af17f2c0c89b7cae32889
ba50 <barjas02@gmail.com>
2017-04-01 08:19:03 +0200

    update

--- lab3.py
+++ lab3.py
@@ -9,15 +9,7 @@ E_cut = 12
 npw = int(sqrt(E_cut/(2*pi/a)**2)+.5)
 npw = 2*npw + 1
 
-G = np.fromfunction()
-print(G)
-"""
-for i in np.arange(0, npw):
-    G.append(i*pi/a)
-"""
-print(G)
-
-G = np.array(G, dtype=float)
+G = np.fromfunction(lambda n, m: (2*pi/a)*n, (npw, 1))
 K = np.arange(-pi/a, pi/a, (2*pi/a)/30, dtype=float)
 H = np.zeros((npw, npw), dtype=float)
 
@@ -26,6 +18,8 @@ for k in K:
     for index, h in np.ndenumerate(H):
         if index[0] == index[1]:
             H[index] = (k+G[index[0]])**2-V_0/(a*b)
-        if not index[0] == index[1]:
+        else:
             H[index] = -V_0/a*sin((G[index[1]]-G[index[0]])*b/2)/((G[index[1]]-G[index[0]])/2)
     H_K.append(H)
+
+print(H_K[0])
